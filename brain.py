5eebed62fd5171769df19d9affe857ae55c3f833
dario <dario3desing@gmail.com>
2021-08-23 23:56:30 +0200

    NN functions added

--- brain.py
+++ brain.py
@@ -4,6 +4,7 @@ from torch import nn
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
 from torchvision import transforms
+import numpy as np
 
 class Brain(nn.Module):
     '''
@@ -13,7 +14,7 @@ class Brain(nn.Module):
         super().__init__()
         # Create neural net which receives a 14-dimensional input vector.
         # and outputs a 4-dimensional vector.
-        self.layers = nn.Sequential(
+        self.model = nn.Sequential(
             nn.Linear(14, 20),
             nn.ReLU(),
             nn.Linear(20, 20),
@@ -23,7 +24,7 @@ class Brain(nn.Module):
 
     def forward(self, x):
         '''Forward pass'''
-        return self.layers(x)
+        return self.model(x)
 
     def decide(self, snake_data):
         '''
@@ -36,6 +37,44 @@ class Brain(nn.Module):
         y = self.forward(x)
         # Return the index of the largest value.
         return int(y.argmax())
+
+    def get_weights(self):
+        return self.model.parameters()
+
+    def get_flattened_weights(self):
+        weights = self.get_weights()
+        return torch.cat([w.view(-1) for w in weights])
+
+    # This method recives a 1D tensor and resape it to the shape of the model weights.
+    def unflatten(self, tensor):
+        unflattened = []
+        shapes = [x.shape for x in self.model.parameters()]
+        cut_index = 0
+        for shape in shapes:
+            size = np.product(shape)
+            unflattened.append(tensor[cut_index:cut_index+size].view(shape))
+            cut_index += size
+        return unflattened
+
+    def set_weights(self, weights):
+        # Check if the weights are the correct shape.
+        if isinstance(weights, torch.Tensor):
+            weights = self.unflatten(weights)
+        # Set the weights to the moodel.
+        for param, w in zip(self.model.parameters(), weights):
+            param.data = nn.parameter.Parameter(w)
+
+
+
+
+
+
+        
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from snake import Snake
 from datetime import datetime
 from multiprocessing import Pool
 from game import draw, listen_events, game_start
+import random
+import numpy as np
 
 
 
@@ -13,8 +15,6 @@ GENERATIONS = 5
 
 # Multiprocessing config
 PARALLEL = True
-
-
 
 
 
@@ -36,6 +36,35 @@ def play_game(snake, show=False):
                 break
 
 
+def crossover(snake1, snake2):
+    ''' Given 2 parents generates 2 children.
+    '''
+    genes1 = snake1.brain.get_flattened_weights()
+    genes2 = snake2.brain.get_flattened_weights()
+
+    split_index = random.rangendint(0, len(genes1)-1)
+
+    child1_genes = genes1[:split_index] + genes2[split_index:]
+    child2_genes = genes2[:split_index] + genes1[split_index:]
+
+    child1 = Snake()
+    child2 = Snake()
+
+    child1.brain.set_weights(child1_genes)
+    child2.brain.set_weights(child2_genes)
+
+    return child1, child2
+
+
+def mutate(snake):
+    ''' Randomly modifies a random picked weight '''
+    flattened = snake.brain.get_flattened_weights()
+    random_index = random.randint(0, len(flattened)-1)
+    flattened[random_index] = np.random.randn()
+    snake.brain.set_weights(flattened)
+    return snake
+
+
 
 if __name__ == '__main__':
     if PARALLEL:
@@ -51,7 +80,6 @@ if __name__ == '__main__':
         print(f'Generation {g} starting at {time_start.strftime("%H:%M:%S")}')
 
         if PARALLEL:
-            pass
             snakes = pool.map(play_game, snakes)
         else:
             for snake in snakes:
@@ -63,10 +91,24 @@ if __name__ == '__main__':
         print(f'Best snake has a score of {snakes[0].score()} with {snakes[0].movements} movements and a length of {len(snakes[0].body)}')
         
         # Select survivors
+        n_survivors = int(SURVIVAL_RATE * POPULATION)
+        survivors = snakes[:n_survivors]
 
-        # Randomly select survivors
+        # Breed new population
+        new_population = []
+        for _ in range(POPULATION - n_survivors):
+            parent1 = survivors[int(n_survivors * (1 - MUTATION_RATE))]
+            parent2 = survivors[int(n_survivors * (1 - MUTATION_RATE))]
+            child = parent1.breed(parent2)
+            new_population.append(child)
 
-        
+        # Mutate new population
+        for snake in new_population:
+            snake.mutate(MUTATION_RATE)
+
+        # Replace old population with new population
+        snakes = survivors + new_population
+
         # Print elapsed time
         time_end = datetime.now()
         print(f'Generation {g} ended in {time_end.minute - time_start.minute} minutes, {time_end.second - time_start.second} second\n')
